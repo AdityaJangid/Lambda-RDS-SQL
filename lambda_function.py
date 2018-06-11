ac51b09f6727032663e905f887b14f46178da433
AdityaJangid <eradityajangid@gmail.com>
2018-06-11 18:48:45 +0530

    lambda function

---
+++ lambda_function.py
@@ -0,0 +1,36 @@
+import sys
+import logging
+import pymysql
+#rds settings
+rds_host  = "mydbinstance.c2ufqyoeq3be.us-east-1.rds.amazonaws.com"
+name = "aditya"
+password = "aditya2018"
+db_name = "punchhDB"
+table_name = 'punchh'
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+try:
+    conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=10)
+except:
+    logger.error("ERROR: Unexpected error: Could not connect to MySql instance.")
+    sys.exit()
+
+logger.info("SUCCESS: Connection to RDS mysql instance succeeded")
+def handler(event, context):
+    """
+    This function fetches content from mysql RDS instance
+    """
+
+    with conn.cursor() as cur:
+        # cur.execute("create table %s ( EmpID  int NOT NULL, Name varchar(255) NOT NULL, PRIMARY KEY (EmpID))" % (table_name))
+        # cur.execute('insert into %s (EmpID, Name) values(1, "Punchh1")'% (table_name))
+        # cur.execute('insert into %s (EmpID, Name) values(2, "punchh2")'% (table_name))
+        # cur.execute('insert into %s (EmpID, Name) values(3, "punchh3")'% (table_name))
+        # conn.commit()
+        # cur.execute("select * from %s"% (table_name))
+        cur.execute("select count(*) from %s" %(table_name))
+        for row in cur:
+            print(row)
